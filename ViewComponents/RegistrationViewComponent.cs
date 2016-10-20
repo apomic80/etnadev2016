3c39900a6210b99bfe7b29fcaf6f069b05d90f06
apomic80 <apomic80@hotmail.com>
2016-10-20 17:49:07 +0200

    fix view component to use tag helpers

--- ViewComponents/RegistrationViewComponent.cs
+++ ViewComponents/RegistrationViewComponent.cs
@@ -1,7 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EtnaDev.Components
+namespace EtnaDev.ViewComponents
 {
     public class RegistrationViewComponent : ViewComponent
     {
